3d7b398bd05a82e51b4b5de807e97dde82ec6452
vfxjamer <arihantkatiyar66@gmail.com>
2026-08-18 12:52:01 +0530

    Update R3maJ.h

--- src/R3maJ.h
+++ src/R3maJ.h
@@ -94,10 +94,10 @@ inline EnvCreateResult EnvCreateFunc(int /*index*/) {
     // ============================================================
     std::vector<WeightedReward> components;
     components.reserve(4);
-    components.emplace_back(new SimpleTouchReward(), 60.f);
+    components.emplace_back(new SimpleTouchReward(), 55.f);
     components.emplace_back(new SpeedTowardBallReward(), 15.f);
-    components.emplace_back(new FaceBallReward(), 10.f);
-    components.emplace_back(new GuideAirReward(), 0.15f);
+    components.emplace_back(new FaceBallReward(), 1.f);
+    components.emplace_back(new GuideAirReward(), 0.25f);
 
     auto* combined = new AllRewardsWrapper(components, g_rewards.opponent_punish_w);
     g_activeRewardsWrapper = combined;
